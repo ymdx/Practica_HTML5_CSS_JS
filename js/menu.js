7624ace3f74efd3eed4591e1173a86a9e4532bc1
Yoel Macia <yoelmaciadelgado@gmail.com>
2017-01-12 11:08:30 +0100

    Added scroll spy to the navbar

--- js/menu.js
+++ js/menu.js
@@ -30,11 +30,11 @@ function scrollToElement(element) {
     document.body.scrollTop += jump;
 
     if (!element.lastJump ||  element.lastJump > Math.abs(jump)) {
-    	element.lastJump = Math.abs(jump);
+        element.lastJump = Math.abs(jump);
 
-    	setTimeout(function(){
-    		scrollToElement(element);
-    	}, "60");
+        setTimeout(function() {
+            scrollToElement(element);
+        }, "60");
     } else {
         element.lastJump = null;
     }
@@ -44,5 +44,69 @@ function scrollToElement(element) {
 function deleteActiveClass() {
     for (var i = 0; i < navbaritems.length; i++) {
         navbaritems[i].classList.remove("active");
+    }
+}
+
+// Scroll Spy
+
+var cumulativeOffset = function(element) {
+    var top = 0;
+    do {
+        top += element.offsetTop || 0;
+        element = element.offsetParent;
+    } while (element);
+    return top;
+};
+
+var offsetQuienSoy = cumulativeOffset(document.getElementById("quien-soy")) - 51;
+var offsetEstudios = cumulativeOffset(document.getElementById("estudios")) - 51;
+var offsetExperiencia = cumulativeOffset(document.getElementById("experiencia")) - 51;
+var offsetSobreMi = cumulativeOffset(document.getElementById("sobre-mi")) - 51;
+
+window.addEventListener("scroll", changeMenuStyle);
+
+function changeMenuStyle(event) {
+    var previous;
+
+    if (window.pageYOffset >= 0 && window.pageYOffset < offsetQuienSoy) {
+        if (!previous) {
+            previous = 1;
+        } else if (previous == 1) {
+            return false;
+        }
+        deleteActiveClass();
+        document.querySelector('a[href="#"]').parentNode.classList.add("active");
+    } else if (window.pageYOffset >= offsetQuienSoy && window.pageYOffset < offsetEstudios) {
+        if (!previous) {
+            previous = 2;
+        } else if (previous == 2) {
+            return false;
+        }
+        deleteActiveClass();
+        document.querySelector('a[href$="quien-soy"]').parentNode.classList.add("active");
+    } else if (window.pageYOffset >= offsetEstudios && window.pageYOffset < offsetExperiencia) {
+        if (!previous) {
+            previous = 3;
+        } else if (previous == 3) {
+            return false;
+        }
+        deleteActiveClass();
+        document.querySelector('a[href$="estudios"]').parentNode.classList.add("active");
+    } else if (window.pageYOffset >= offsetExperiencia && window.pageYOffset < offsetSobreMi) {
+        if (!previous) {
+            previous = 4;
+        } else if (previous == 4) {
+            return false;
+        }
+        deleteActiveClass();
+        document.querySelector('a[href$="experiencia"]').parentNode.classList.add("active");
+    } else if (window.pageYOffset >= offsetSobreMi) {
+        if (!previous) {
+            previous = 5;
+        } else if (previous == 5) {
+            return false;
+        }
+        deleteActiveClass();
+        document.querySelector('a[href$="sobre-mi"]').parentNode.classList.add("active");
     }
 }
